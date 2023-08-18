63f61dec162808b22710f8c1864deb3926b934af
Ionut Ivanciu <ionutziv7@protonmail.com>
2023-08-18 22:59:17 +0300

    Shopping cart and media queries

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ const searchIcon = document.querySelector("#search-icon");
 const shoppingIcon = document.querySelector("#shopping-icon");
 const menuIcon = document.querySelector("#menu-icon");
 const searchBox = document.querySelector(".search-box");
+const navbar = document.querySelector(".header .navbar");
+const shoppingCart = document.querySelector(".shopping-cart-container");
 
 searchIcon.onclick = () => {
-  searchBox.classList.toggle('active');
+  searchBox.classList.toggle("active");
+  navbar.classList.remove("active");
+  shoppingCart.classList.remove("active");
+};
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle("active");
+  searchBox.classList.remove("active");
+  shoppingCart.classList.remove("active");
+};
+
+shoppingIcon.onclick = () => {
+  shoppingCart.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchBox.classList.remove("active");
 };
