3767742189e60500246733e320443fac115d1493
Ionut Ivanciu <ionutziv7@protonmail.com>
2023-08-28 18:04:16 +0300

    Change header menu position and active links

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const searchBox = document.querySelector(".search-box");
 const navbar = document.querySelector(".header .navbar");
 const shoppingCart = document.querySelector(".shopping-cart-container");
 const toTop = document.querySelector("#to-top");
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".header .navbar a");
 
 searchIcon.onclick = () => {
   searchBox.classList.toggle("active");
@@ -13,6 +15,7 @@ searchIcon.onclick = () => {
 };
 
 menuIcon.onclick = () => {
+  // menuIcon.classList.toggle('fa-times');
   navbar.classList.toggle("active");
   searchBox.classList.remove("active");
   shoppingCart.classList.remove("active");
@@ -25,10 +28,28 @@ shoppingIcon.onclick = () => {
 };
 
 window.onscroll = () => {
+  // menuIcon.classList.remove("fa-times");
   shoppingCart.classList.remove("active");
   navbar.classList.remove("active");
   searchBox.classList.remove("active");
   toTop.classList.toggle("active", window.scrollY > 300);
+
+  // links active
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector(".header .navbar a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 };
 
 // Swiper
